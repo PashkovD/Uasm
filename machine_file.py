be7334271b4aa04fac315dee7ade58d10ac54d5e
PashkovD <dimon2006763@gmail.com>
2022-12-06 19:20:26 +0500

    Added apply_relocations to MachineFile

--- machine_file.py
+++ machine_file.py
@@ -1,4 +1,6 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Tuple
+
+from typeguard import typechecked
 
 from not_int import NotInt
 
@@ -7,16 +9,22 @@ class MachineFile:
     def __init__(self):
         self.data = bytearray()
         self.symbols: Dict[str, int] = {}
-        self.relocations: Dict[int, List[str]] = {}
+        self.relocations: List[Tuple[int, str, int]] = []
 
     @property
     def current_pos(self) -> int:
         return len(self.data)
 
+    @typechecked
     def write(self, data: Union[bytearray, bytes]) -> None:
-        self.data += data
+        self.data.extend(data)
 
+    @typechecked
     def write_int(self, num: NotInt) -> None:
         for i, f in num.symbols.items():
-            self.relocations[self.current_pos] = self.relocations.get(self.current_pos, []) + [i] * f
-        self.write(bytes((num.num % 256 + 256) % 256, ))
+            self.relocations.append((self.current_pos, i, f))
+        self.data.append((num.num % 256 + 256) % 256)
+
+    def apply_relocations(self) -> None:
+        for i, f, j in self.relocations:
+            self.data[i] = ((self.data[i] + self.symbols[f] * j) % 256 + 256) % 256
