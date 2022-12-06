60593dbc3a1842da5a0676c4abb4efd20067cff4
PashkovD <dimon2006763@gmail.com>
2022-12-06 19:03:18 +0500

    Added write_int to MachineFile

--- machine_file.py
+++ machine_file.py
@@ -1,5 +1,7 @@
 from typing import Dict, List, Union
 
+from not_int import NotInt
+
 
 class MachineFile:
     def __init__(self):
@@ -11,5 +13,10 @@ class MachineFile:
     def current_pos(self) -> int:
         return len(self.data)
 
-    def write(self, data: Union[bytearray, bytes]):
+    def write(self, data: Union[bytearray, bytes]) -> None:
         self.data += data
+
+    def write_int(self, num: NotInt) -> None:
+        for i, f in num.symbols.items():
+            self.relocations[self.current_pos] = self.relocations.get(self.current_pos, []) + [i] * f
+        self.write(bytes((num.num % 256 + 256) % 256, ))
